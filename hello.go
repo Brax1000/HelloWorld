7276ec6b0c4d227829ee6cbb30d9bc20d4be7970
C.C <braxtenorlineurdelas@gmail.com>
2026-09-15 19:26:50 +0800

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,11 @@
+// Go — run:  go run hello.go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("I'm Braxten Orline Urdelas!")
+	fmt.Println("Someday I will become a successful Professional")
+	fmt.Println("and be known for my skills and intellect.")
+}
